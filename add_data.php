43dbc70a7c0bd90d23155e462821f3349b77547e
ivykoko1 <ramon.sanchez68@gmail.com>
2017-11-14 00:06:46 +0100

    Primer commit

---
+++ add_data.php
@@ -0,0 +1,37 @@
+<?php
+    $conexion = new mysqli("localhost", "root" ,"" , "datos_fitness");
+     
+    //Comprobar si hay error al conectar
+
+    if($conexion->connect_errno){
+        echo 'Error en la conexion SQL: ' . $conexion->connect_error;
+    }
+
+    $test = $conexion->query("SET NAMES 'utf8'");
+
+    if(isset($_POST['add'])){
+        add($conexion);
+    }
+
+    function add($conexion){
+        $id_actividad = $_POST['tipoActividad'];
+        $duracion = $_POST['duracion'];
+        $distancia = $_POST['distancia'];
+        $intensidad;
+        $insertQuery;
+
+        if(isset($_POST['intensidad'])){
+            $intensidad = $_POST['intensidad'];
+        }
+
+        $query = $conexion->query('SELECT intensidad FROM actividad WHERE id='.$id_actividad);
+        $hasIntensidad = $query->fetch_assoc()['intensidad'];
+        if($hasIntensidad){
+            $insert = $conexion->query('INSERT INTO entrenamiento(duracion, distancia, intensidad, id_actividad) VALUES ('.$duracion.', '.$distancia.', '.$intensidad.', '. $id_actividad.')'); 
+        }else{
+            $insert = $conexion->query('INSERT INTO entrenamiento(duracion, distancia, id_actividad) VALUES ('.$duracion.', '.$distancia.', '.$id_actividad.')'); 
+        }
+        header("Location: index.php?sucess=true ",true,303);  
+        exit();  
+    }
+?>
