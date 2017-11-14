3ec73be3e2837283c03f81bbbfb90c61de0edac3
Ivan sanchez <ivykoko1>
2017-11-14 14:33:06 +0100

    Añadido html de registro Signed-off-by: Ivan sanchez <ivykoko1>

---
+++ register.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Registrarse</title>
+  </head>
+  <body>
+    <form method="post" action="createUser.php">
+      Nombre de Usuario: <input type="text" name="username"></input><br>
+      Nombre: <input type="text" name="name"></input><br>
+      Contraseña: <input type="password" name="pwd"></input><br>
+      Confirmar contraseña: <input type="password" name="pwd2"></input>
+      <input type="submit" name="submit" value="Registrarse"></input>
+    </form>
+  </body>
+  </html>
